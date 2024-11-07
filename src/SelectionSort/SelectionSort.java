9969704e042f948fbe4923f20885830e6601dfeb
gurumdevv <gurumdevv@gmail.com>
2024-11-07 17:36:37 +0900

    study: 단순 선택 정렬 구현

---
+++ src/SelectionSort/SelectionSort.java
@@ -0,0 +1,43 @@
+package SelectionSort;
+
+import java.util.Scanner;
+
+class SelectionSort {
+
+    static void swap(int[] a, int index1, int index2) {
+        int temp = a[index1];
+        a[index1] = a[index2];
+        a[index2] = temp;
+    }
+
+    static void selectionSort(int[] array, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[min]) min = j;
+            }
+            swap(array, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("단순 삽입 정렬");
+        System.out.print("요솟수: ");
+        int size = stdIn.nextInt();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("x[" + i + "]: ");
+            array[i] = stdIn.nextInt();
+        }
+
+        selectionSort(array, size);
+
+        System.out.println("오름차순으로 정렬 완료!");
+        for (int i = 0; i < size; i++) {
+            System.out.println("x[" + i + "]: " + array[i]);
+        }
+    }
+}
