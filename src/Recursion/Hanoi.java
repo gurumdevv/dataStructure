54bdb7e34e4a1f85fea9bb93c77b775c7f03c60e
gurumdevv <gurumdevv@gmail.com>
2024-11-24 18:47:38 +0900

    study: 하노이 구현 주석 추가

--- src/Recursion/Hanoi.java
+++ src/Recursion/Hanoi.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Hanoi {
 
     static void move(int no, int x, int y) {
-        if (no > 1) move(no - 1, x, 6 - x - y);
+        if (no > 1) move(no - 1, x, 6 - x - y); //x: 시작 기둥, y: 목표 기둥: 보조기둥: 6- x - y, n-1개 원판를 시작 -> 보조
 
-        System.out.printf("원반[%d]을(를) %d번 기둥에서 %d번 기둥으로 옮김\n", no, x, y);
+        System.out.printf("원반[%d]을(를) %d번 기둥에서 %d번 기둥으로 옮김\n", no, x, y); //바닥 원판을 시작 -> 목표
 
-        if (no > 1) move(no - 1, 6 - x - y, y);
+        if (no > 1) move(no - 1, 6 - x - y, y); //x: 시작 기둥, y: 목표 기둥: 보조기둥: 6- x - y, n-1개 원판을 보조 -> 목표
     }
 
     public static void main(String[] args) {
